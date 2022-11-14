a6a48ceb1e3d6d689ea6c260e717e29bdb956a8f
Adrien <Adnn@users.noreply.github.com>
2022-11-14 10:05:24 +0100

    Add stutter-noglfw minimal win32 app, set requirements versions.

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -22,10 +22,10 @@ class EntityprotosConan(ConanFile):
     }
 
     requires = (
-        "entity/game2",
-        "graphics/game2",
-        "handy/game2",
-        "math/game2",
+        "entity/83e1ade064@adnn/develop",
+        "graphics/aa960a2385@adnn/develop",
+        "handy/acd90c0549@adnn/develop",
+        "math/541fce5f6a@adnn/develop",
     )
 
     # Note: It seems conventionnal to add CMake build requirement
